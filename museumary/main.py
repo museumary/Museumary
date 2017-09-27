b6e3a3722340bdbdbc44fc547a4799b5f596b325
Kenny Le <kennyle1412@gmail.com>
2017-09-27 00:51:32 -0500

    added .html routes in main. fixed path in home.html.

--- museumary/main.py
+++ museumary/main.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/home.html')
 def index():
     return render_template('home.html')
 
+
 @app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template('about.html')
-    
+
+
+# This is only used when running locally. When running live, gunicorn runs
+# the application.
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
