1f22229756ab40b66dcb1022ed410643c5602505
TrentonStrahan <tstray08@gmail.com>
2017-10-21 17:58:58 -0500

    Added Carousel

--- museumary-react/src/App.js
+++ museumary-react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import Carousel from './Carousel';
 
 const App = () => {
   return (<div className="App">
             <Header />
+            <Carousel />
           </div>);
 };
 
--- museumary-react/src/Carousel.js
+++ museumary-react/src/Carousel.js
@@ -2,7 +2,36 @@ import React from 'react';
 
 const Carousel = () => {
   return (<div className="Carousel">
-            // TODO: Implement Carousel
+		      <div id="myCarousel" className="carousel slide" data-ride="carousel">
+		        
+		        <ol className="carousel-indicators">
+		          <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+		          <li data-target="#myCarousel" data-slide-to="1"></li>
+		          <li data-target="#myCarousel" data-slide-to="2"></li>
+		        </ol>
+
+		        
+		        <div className="carousel-inner">
+		          <div className="item active">
+		          </div>
+
+		          <div className="item">
+		          </div>
+
+		          <div className="item">
+		          </div>
+		        </div>
+
+		        
+		        <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+		          <span className="glyphicon glyphicon-chevron-left"></span>
+		          <span className="sr-only">Previous</span>
+		        </a>
+		        <a className="right carousel-control" href="#myCarousel" data-slide="next">
+		          <span className="glyphicon glyphicon-chevron-right"></span>
+		          <span className="sr-only">Next</span>
+		        </a>
+		      </div>
           </div>);
 };
 
