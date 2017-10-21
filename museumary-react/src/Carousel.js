e3078a562e00d13947b85ffd6538682a8c8df2f1
maxswest <max.west@utexas.edu>
2017-10-21 16:39:11 -0500

    added in basics for react app

---
+++ museumary-react/src/Carousel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Carousel = () => {
+  return (<div className="Carousel">
+            // TODO: Implement Carousel
+          </div>);
+};
+
+export default Carousel;
