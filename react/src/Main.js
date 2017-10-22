b7b1903a9a6327126f0aeed3240a92b2984f8f48
maxswest <max.west@utexas.edu>
2017-10-22 17:02:18 -0500

    added basics for about page

--- react/src/Main.js
+++ react/src/Main.js
@@ -5,6 +5,7 @@ import Artists from './Artists';
 import Venues from './Venues';
 import Types from './Types';
 import Works from './Works';
+import About from './About';
 
 const Main = () => {
   return (<div className='Main'>
@@ -14,6 +15,7 @@ const Main = () => {
               <Route path='/venues' component={Venues}/>
               <Route path='/types' component={Types}/>
               <Route path='/works' component={Works}/>
+              <Route path='/about' component={About}/>
             </Switch>
           </div>);
 };
