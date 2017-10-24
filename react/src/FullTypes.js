f47a8fae4046719b8b6d9f8bcc786333f2961b5e
maxswest <max.west@utexas.edu>
2017-10-23 19:27:31 -0500

    added nested routing for all data models

---
+++ react/src/FullTypes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FullTypes = () => {
+  return (<div className="FullTypes">
+            This is where the types will go
+          </div>);
+};
+
+export default FullTypes;
