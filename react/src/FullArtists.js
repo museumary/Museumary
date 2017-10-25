7e00a7ff365efe05f517d869e822bf0fec533ebc
maxswest <max.west@utexas.edu>
2017-10-25 17:08:03 -0500

    added linking and listing to the artists page

--- react/src/FullArtists.js
+++ react/src/FullArtists.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import ArtistLink from './ArtistLink';
 
 class FullArtists extends React.Component {
 	constructor() {
   	super();
  		 this.state={items:[]};
   }
-  componentWillMount(){
+  componentDidMount(){
   	fetch(`http://museumary.me/api/artist`)
  		.then(result=>result.json())
     .then(items=>this.setState({items}))
   }
+
+
   render() {
-  	return(
-      <div className="FullArtists">
-        This is where the artists will go
-      </div>
-   )
-  }
+		if(this.state.items.objects){
+			var arr = [];
+			console.log("THINGS IN OBJECTS:");
+			console.log(this.state.items.objects);
+			this.state.items.objects.forEach(function(obj) {
+				arr.push(obj);
+			});
+			return <div className="FullArtists">
+							{
+								arr.map(
+									function(obj) {
+										console.log(obj.id);
+										console.log(obj.name);
+										return <ArtistLink id={obj.id} name={obj.name} />;
+									}
+								)
+							}
+						</div>;
+		}
+		else {
+			return <div className="FullArtists"></div>;
+		}
+	}
 }
 
 export default FullArtists;
