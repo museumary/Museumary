5a93ad819ebd02c35cfde2ae6e71119f560b3126
maxswest <max.west@utexas.edu>
2017-10-25 23:12:31 -0500

    data fetching for types and works

--- react/src/FullTypes.js
+++ react/src/FullTypes.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
-const FullTypes = () => {
-  return (<div className="FullTypes">
-            This is where the types will go
-          </div>);
-};
+class FullTypes extends React.Component {
+	constructor() {
+  	super();
+ 		 this.state={items:[]};
+  }
+  componentDidMount(){
+  	fetch(`http://museumary.me/api/type`)
+ 		.then(result=>result.json())
+    .then(items=>this.setState({items}))
+  }
+
+
+  render() {
+		if(this.state.items.objects){
+			var arr = [];
+			this.state.items.objects.forEach(function(obj) {
+				arr.push(obj);
+			});
+			return <div className="FullTypes">
+							{
+								arr.map(
+									function(obj) {
+                    var url = '/types/' + obj.id;
+										return <div><a href={url}>{obj.name}</a><br/></div>;
+									}
+								)
+							}
+						</div>;
+		}
+		else {
+			return <div className="FullTypes"></div>;
+		}
+	}
+}
 
 export default FullTypes;
