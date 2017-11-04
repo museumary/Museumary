085be9d65665875bc207ee2f623718eacd094ab5
maxswest <max.west@utexas.edu>
2017-11-04 13:44:58 -0500

    changed all tabs to spaces

--- react/src/components/FullTypes.js
+++ react/src/components/FullTypes.js
@@ -4,48 +4,48 @@ import style from './Full.css';
 
 
 class FullTypes extends React.Component {
-	constructor() {
-  	super();
- 		 this.state={items:[]};
-  }
-  componentDidMount(){
-  	fetch(`http://api.museumary.me/art_type?entries_per_page=80`)
- 		.then(result=>result.json())
-    .then(items=>this.setState({items}))
-  }
+    constructor() {
+        super();
+        this.state={items:[]};
+    }
+    componentDidMount(){
+        fetch(`http://api.museumary.me/art_type?entries_per_page=80`)
+            .then(result=>result.json())
+            .then(items=>this.setState({items}))
+    }
 
 
-  render() {
-		if(this.state.items.objects){
-			var arr = [];
-			this.state.items.objects.forEach(function(obj) {
-				arr.push(obj);
-			});
-			return <div className="FullTypes">
-						<div className="container">
-							<div className="row">
-								{
-									arr.map(
-										function(obj) {
-	                    					var url = '/types/' + obj.id;
-											return <div className="col-md-3">
-														<Link to={url} activeClassName="active"><strong>{obj.name}</strong></Link>
-														<br/>
-														<br/>
-													</div>;
-										}
-									)
-								}
-							</div>
-							<br/>
-							<br/>
-						</div>
-					</div>;
-		}
-		else {
-			return <div className="FullTypes"></div>;
-		}
-	}
+    render() {
+        if(this.state.items.objects){
+            var arr = [];
+            this.state.items.objects.forEach(function(obj) {
+                arr.push(obj);
+            });
+            return <div className="FullTypes">
+                        <div className="container">
+                            <div className="row">
+                                {
+                                    arr.map(
+                                        function(obj) {
+                                            var url = '/types/' + obj.id;
+                                            return <div className="col-md-3">
+                                                        <Link to={url} activeClassName="active"><strong>{obj.name}</strong></Link>
+                                                        <br/>
+                                                        <br/>
+                                                </div>;
+                                        }
+                                    )
+                                }
+                            </div>
+                            <br/>
+                            <br/>
+                        </div>
+                    </div>;
+        }
+        else {
+            return <div className="FullTypes"></div>;
+        }
+    }
 }
 
 export default FullTypes;
