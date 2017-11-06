534dfc600eeb023356841467ce9890baa5d4ca13
Kenny Le <kennyle1412@gmail.com>
2017-11-05 23:47:49 -0600

    added basic pagination logic to full artists.

--- react/src/components/FullArtists.js
+++ react/src/components/FullArtists.js
@@ -2,23 +2,92 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './Full.css';
 import Thumbnail from './Thumbnail'
+import Pagination from './Pagination'
 
 class FullArtists extends React.Component {
+    static defaultProps = {
+        initialPage: 1,
+        entries_per_page: 16,
+        url: 'http://api.museumary.me/artist'
+    }
+
     constructor() {
         super();
         this.state={
-            items:[],
-            activePage: 1
+            items: [],
+            displayItems: [],
+            activePage: 1,
+            numPages: 0,
+            loading: true
         };
     }
 
     componentDidMount() {
-        fetch(`http://api.museumary.me/artist?entries_per_page=24`)
-            .then(result=>result.json())
+        const num_entries = this.props.entries_per_page
+
+        fetch(`http://api.museumary.me/artist?entries_per_page=`+num_entries)
+            .then(result=>{
+                return result.json()
+            })
             .then(items=> {
-                this.setState({ items })
+                const numPages = items.info.num_pages;
+                this.setState({ items: items, numPages: numPages })
             });
     }
+
+    incPage = () => {
+        console.log('inc page')
+
+        if(this.state.activePage < this.state.numPages) {
+            this.setState({ activePage: this.state.activePage + 1 })
+        }
+        else {
+            console.log('not incrementing. ')
+            console.log('active ' + this.state.activePage)
+            console.log('numpages: ' + this.state.numPages)
+        }
+    };
+
+    decPage = () => {
+        console.log('dec page.')
+
+        if(this.state.activePage > 1) {
+            this.setState({ activePage: this.state.activePage - 1 })
+        }
+        else {
+            console.log('not dec. ')
+            console.log('active ' + this.state.activePage)
+            console.log('numpages: ' + this.state.numPages)
+        }
+    };
+
+    firstPage = () => {
+        console.log('f page.')
+
+        if(this.state.activePage != this.props.initialPage) {
+            this.setState({ activePage: this.props.initialPage })
+        }
+        else {
+            console.log('not resetting. ')
+            console.log('active ' + this.state.activePage)
+            console.log('numpages: ' + this.state.numPages)
+        }
+    };
+
+    lastPage = () => {
+        if(this.state.activePage != this.state.numPages) {
+            this.setState({ activePage: this.state.numPages })
+        }
+        else {
+            console.log('not advancing. ')
+            console.log('active ' + this.state.activePage)
+            console.log('numpages: ' + this.state.numPages)
+        }
+    };
+
+    changePage = () => {
+        console.log('c page.')
+    };
 
     render() {
         if(this.state.items.objects){
@@ -37,6 +106,14 @@ class FullArtists extends React.Component {
                         <br/>
                         <br/>
                    </div>
+                   <Pagination
+                        activePage={this.state.activePage}
+                        numPages={this.state.numPages}
+                        incPage={this.incPage}
+                        decPage={this.decPage}
+                        firstPage={this.firstPage}
+                        lastPage={this.lastPage}
+                   />
                </div>
            );
         }
--- react/src/components/Pagination.js
+++ react/src/components/Pagination.js
@@ -1,59 +1,40 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import { Link } from 'react-router-dom';
+import style from './Pagination.css'
 
-export class Pagination extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            activePage: props.initialPage
-        }
-    }
+export default class Pagination extends React.Component {
+    static defaultProps = {
+        defaultButtonText: [1, 2, 3, 4, 5],
+        buttonIndexs: [-2, -1, 0, 1, 2]
+    };
 
     render() {
+        let buttonText = []
+        let baseNumber = this.props.activePage
+
+        if(baseNumber < 3) {
+            buttonText = this.props.defaultButtonText
+        }
+        else {
+            if(baseNumber > this.props.numPages - 2) {
+                baseNumber = this.props.numPages - 2;
+            }
+
+            buttonText = this.props.buttonIndexs.map(index => baseNumber+index)
+        }
+
+        buttonText = buttonText.map(number => {
+            return <button type='button' className={number === this.props.activePage ? 'active' : ''} key={number}>{number}</button>
+        })
+
         return (
             <ul>
-                <button type="button">{"<<"}</button>
-                <button type="button">{"<"}</button>
-                <button type="button">1</button>
-                <button type="button">2</button>
-                <button type="button">3</button>
-                <button type="button">4</button>
-                <button type="button">5</button>
-                <button type="button">></button>
-                <button type="button">>></button>
+                <button type="button" onClick={this.props.firstPage}>{"<<"}</button>
+                <button type="button" onClick={this.props.decPage}>{"<"}</button>
+                {buttonText}
+                <button type="button" onClick={this.props.incPage}>></button>
+                <button type="button" onClick={this.props.lastPage}>>></button>
             </ul>
         );
-    }
+    };
 }
-
-// class Pagination extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state={
-//             items:[],
-//             activePage: props.initialPage
-//         };
-//     }
-
-//     render() {
-//         return (
-//             <ul>
-//                 <button type="button">{"<<"}</button>
-//                 <button type="button">{"<"}</button>
-//                 <button type="button">1</button>
-//                 <button type="button">2</button>
-//                 <button type="button">3</button>
-//                 <button type="button">4</button>
-//                 <button type="button">5</button>
-//                 <button type="button">></button>
-//                 <button type="button">>></button>
-//             </ul>
-//         );
-//     }
-// }
-
-Pagination.defaultProps = {
-    initialPage: 1
-}
-
-export default Pagination
